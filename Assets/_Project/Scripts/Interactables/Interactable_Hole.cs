3a9ca26af60c95a9843247ced9510da40b5afe6c
HJYIMN0 <sim0allaseconda@gmail.com>
2026-04-19 04:00:37 +0200

    Interactable Hole started. Created Day 01 scene.

--- Assets/_Project/Scripts/Interactables/Interactable_Hole.cs
+++ Assets/_Project/Scripts/Interactables/Interactable_Hole.cs
@@ -2,10 +2,26 @@ using UnityEngine;
 
 public class Interactable_Hole : AbstractInteractable
 {
-
+    [SerializeField] private GameObject UiVideoCanva;
     public override void InteractWithTask()
     {
-        throw new System.NotImplementedException();
+        int day = GameFlowManager.Instance.CurrentDay;
+        switch (day)
+        {
+            case 0:
+                Debug.Log("Hole interacted on day 0. No scene change.");
+                break;
+            case 1:
+                Debug.Log("Hole interacted on day 1. No scene change.");
+                break;
+            case 2:
+                Debug.Log("Hole interacted on day 2. No scene change.");
+                break;
+            default:
+                Debug.LogError($"Invalid day index: {day}. Cannot load scene.");
+                break;
+        }
+        Instantiate(UiVideoCanva);
     }
 
 }
